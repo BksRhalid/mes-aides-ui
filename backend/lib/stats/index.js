b1339a7b7099bac7509af281b16b7c9d540b0c50
Alexandre Segura <mex.zktk@gmail.com>
2018-09-26 13:32:11 +0200

    Avoid hardcoding MongoDB DSN, make stats work in dev.

--- backend/lib/stats/index.js
+++ backend/lib/stats/index.js
@@ -15,13 +15,15 @@ var nineWeeksAgo = dateDaysAgo(7 * 9);
 var yesterday = dateDaysAgo(1);
 var today = dateDaysAgo(0);
 
-var relative_path = __dirname + '/../../../dist/documents/stats.json';
+var dirname = process.env.NODE_ENV === 'production' ? __dirname + '/../../../dist' : __dirname + '/../../../app';
+var filename = dirname + '/documents/stats.json';
+
 Promise.all([
     mongodb.getDailySituationCount(nineWeeksAgo,today),
     piwik.getUsageData(nineWeeksAgo, yesterday)
 ])
     .then(function(data) { return [].concat(data[0], data[1]); })
-    .then(function(data) { return fs.writeFileAsync(relative_path, JSON.stringify(data, null, 2), 'utf-8'); })
+    .then(function(data) { return fs.writeFileAsync(filename, JSON.stringify(data, null, 2), 'utf-8'); })
     .catch(function(error) {
         console.error('error', error);
         process.exitCode = 1;
