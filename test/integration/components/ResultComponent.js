21fd2ccbcccba34b98af8be84d48eeb037205ec3
Alexandre Segura <mex.zktk@gmail.com>
2018-12-12 16:42:03 +0100

    Update test selectors.

--- test/integration/components/ResultComponent.js
+++ test/integration/components/ResultComponent.js
@@ -1,14 +1,14 @@
 title: 'h1',
 
-resultatFrame: '.frame-resultats .droits-eligibles',
+resultatFrame: '.droits-eligibles-list',
 
-prestationName: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .description',
-prestationMontant: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .montant',
-prestationMontantDetail: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .montant-detail',
-prestationYM2Warning: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') i.fa-warning',
-togglePrestationTabLink: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') a',
-prestationDescription: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') p',
-prestationMoreInfoLink: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .btn-default',
+prestationName: '.droits-eligibles-list .droits-eligibles-list-item:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .description',
+prestationMontant: '.droits-eligibles-list .droits-eligibles-list-item:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .montant',
+prestationMontantDetail: '.droits-eligibles-list .droits-eligibles-list-item:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .montant-detail',
+prestationYM2Warning: '.droits-eligibles-list .droits-eligibles-list-item:nth-child(' + PRESTATION_TO_TEST_POSITION + ') i.fa-warning',
+togglePrestationTabLink: '.droits-eligibles-list .droits-eligibles-list-item:nth-child(' + PRESTATION_TO_TEST_POSITION + ') a',
+prestationDescription: '.droits-eligibles-list .droits-eligibles-list-item:nth-child(' + PRESTATION_TO_TEST_POSITION + ') p',
+prestationMoreInfoLink: '.droits-eligibles-list .droits-eligibles-list-item:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .btn-default',
 
 openPrimeActiviteLink: { a: 'Prime d’activité' },
 requestPrimeActiviteLink: { a: 'Faire une demande' },
